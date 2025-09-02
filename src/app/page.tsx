c8af0cc119d1df4492a4f0049d1957f2e40e271e
Lincon <linkon789@gmail.com>
2025-09-02 20:42:29 -0300

    feat: remover código comentado e desnecessário do Header; simplificar a estrutura do componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,8 @@ export default function Home() {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
       {/* <Main />
       <Footer /> */}
-=======
-      <Main />
-      <Footer />
->>>>>>> f1fea2b (feat: atualizar README, adicionar estrutura do projeto e instruções de instalação; implementar componentes Header, Main e Footer com estilos)
     </div>
   );
 }
