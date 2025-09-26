b00232c47bd504e834af7986ce2347624eb2f3ca
Lincon <linkon789@gmail.com>
2025-09-26 01:54:45 -0300

    feat: adicionar suppressHydrationWarning ao elemento html para evitar avisos de hidratação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
 }) {
   const session = await getServerSession(authOptions);
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body>
         <Providers session={session}>{children}</Providers>
       </body>
