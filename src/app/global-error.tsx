dd9c5ba49fa42049c71faaaeb1a8083285140570
Lincon <linkon789@gmail.com>
2025-09-29 16:37:15 -0300

    Atualizcao

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -9,7 +9,7 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
+        <h2>Something wqent wrong!</h2>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
